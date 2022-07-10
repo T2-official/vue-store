b366d9bc4089986cdcfe692326cc87085846f10f
T2 <2686757907@qq.com>
2022-07-10 16:21:47 +0800

    fixed 2

--- src/main.js
+++ src/main.js
@@ -29,6 +29,71 @@ Axios.interceptors.response.use(
     if (res.data.code === "401") {
       // 401 Not Loggin
       Vue.prototype.notifyError(res.data.msg);
+      store.dispatch("setShowLogin", true);
     }
+    if (res.data.code === "500") {
+      router.push({ path: "/error" });
+    }
+    return res
+  },
+  error => {
+    router.push({ path: "/error" });
+    return Promise.reject(error);
   }
 )
+
+// Is it Loggin ?
+router.beforeResolve((to, from, next) => {
+  const loginUser = store.state.user.user;
+  if (to.meta.requireAuth) {
+    // requireAuth a meta property in router Showing whether it needs loggin
+    if (!loginUser) {
+      store.dispatch("setShowLogin", true);
+      if (from.name == null) {
+        next("/");
+        return;
+      }
+      next(false);
+      return;
+    }
+  }
+  next();
+})
+
+// time-filter change the timestamp into time
+// y-m-d
+Vue.filter('dateFormat', (dataStr) => {
+  var time = new Date(dataStr);
+  function timeAdd0(str) {
+    if (str < 10) {
+      str = '0' + str;
+    }
+    return str;
+  }
+  let y = time.getFullYear();
+  let m = time.getMonth() + 1;
+  let d = time.getDate();
+  let h = time.getHours();
+  let mm = time.getMinutes();
+  let s = time.getSeconds();
+  return y + '-' + timeAdd0(m) + '-' + timeAdd0(d) + ' ' + timeAdd0(h) + ':' + timeAdd0(mm) + ':' + timeAdd0(s);
+});
+
+// Global components
+import MyMenu from "./components/MyMenu";
+Vue.component(MyMenu.name, MyMenu);
+import MyList from './components/MyList';
+Vue.component(MyList.name, MyList);
+import MyLogin from './components/MyLogin';
+Vue.component(MyLogin.name, MyLogin);
+import MyRegister from './components/MyRegister';
+Vue.component(MyRegister.name, MyRegister);
+
+// !you are running in a develop...
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
