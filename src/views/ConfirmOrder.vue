e22baea6315c9da5369bd2e0472a542fce9cd2f8
T2 <2686757907@qq.com>
2022-07-12 17:21:43 +0800

    fixed 6

---
+++ src/views/ConfirmOrder.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="confirmOrder">
+    <!-- header -->
+    <div class="confirmOrder-header">
+      <div class="header-content">
+        <p>
+          <i class="el-icon-s-order"></i>
+        </p>
+        <p>确认订单</p>
+        <router-link to></router-link>
+      </div>
+    </div>
+    <!-- header end -->
+
+    <!-- main -->
+    <div class="content">
+      <!-- address -->
+      <div class="section-address">
+        <p class="title">收货地址</p>
+        <div class="address-body">
+          <ul>
+            <li :class="item.id == confirmAddress ? 'in-section' : ''" v-for="item in address" :key="item.id">
+              <h2>{{ item.name }}</h2>
+              <p class="phone">{{ item.phone }}</p>
+              <p class="address">{{ item.address }}</p>
+            </li>
+            <li class="add-address">
+              <i class="el-icon-circle-plus-outline"></i>
+              <p>添加新地址</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- goods end discount-->
+      <div class="section-goods">
+        <p class="title">商品及优惠券</p>
+        <div class="goods-list">
+          <ul>
+            <li v-for="item in getCheckGoods" :key="item.id">
+              <img :src="$target + item.producting" />
+              <span class="pro-name">{{ item.productName }}</span>
+              <span class="pro-price">{{ item.price }}元 X {{ item.num }}</span>
+              <span class="pro-status"></span>
+              <span class="pro-total">{{ item.price * item.num }}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- goods and -->
+
+      <!-- delivery  -->
+      <div class="section-shipment">
+        <p class="title">配送方式</p>
+        <p class="shipment">包邮</p>
+      </div>
+      <!-- delivery ends  -->
+
+      <!-- receipt -->
+      <div class="section-invoice">
+        <p class="title">发票</p>
+        <p class="invoice">电子发票</p>
+        <p class="invoice">个人</p>
+        <p class="invoice">商品明细</p>
+      </div>
+      <!-- receipt ends -->
+
+      <!-- calculate -->
+      <div class="section-count">
+        <div class="money-box">
+          <ul>
+            <li>
+              <span class="title">商品件数：</span>
+              <span class="value">{{ getCheckNum }}件</span>
+            </li>
+            <li>
+              <span class="title">商品总价：</span>
+              <span class="value">{{ getTotalPrice }}元</span>
+            </li>
+            <li>
+              <span class="title">活动优惠：</span>
+              <span class="value">-0元</span>
+            </li>
+            <li>
+              <span class="title">优惠券抵扣：</span>
+              <span class="value">-0元</span>
+            </li>
+            <li>
+              <span class="title">运费：</span>
+              <span class="value">0元</span>
+            </li>
+            <li class="total">
+              <span class="title">应付总额：</span>
+              <span class="value">
+                <span class="total-price">{{ getTotalPrice }}</span>元
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- calculate ends -->
+
+      <!-- pay -->
+      <div class="section-bar">
+        <div class="btn">
+          <router-link to="/shoppingCart" class="btn-base btn-return">返回购物车</router-link>
+          <a href="javascript:void(0);" @click="addOrder" class="btn-base btn-primary">结算</a>
+        </div>
+      </div>
+      <!-- pay ends -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style  scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,196 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home" id="home" name="home">
+    <!-- swiper -->
+    <div class="block">
+      <el-carousel height="460px">
+        <el-carousel-item v-for="item in carousel" :key="item.carousel_id">
+          <img style="height:460px;" :src="$target + item.imgPath" :alt="item.describes" />
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <div class="main-box">
+      <div class="main">
+        <!-- phone -->
+        <div class="phone">
+          <div class="box-hd">
+            <div class="title">手机</div>
+          </div>
+          <div class="box-bd">
+            <div class="promp-list">
+              <router-link to>
+                <img :src="$target + 'public/imgs/phone/phone.png'" />
+              </router-link>
+            </div>
+            <div class="list">
+              <MyList :list="phoneList" :isMore="true"></MyList>
+            </div>
+          </div>
+        </div>
+        <!-- phone End -->
+
+        <!-- household electric appliance -->
+        <div class="appliance" id="promo-menu">
+          <div class="box-hd">
+            <div class="title">家电</div>
+            <div class="more" id="more">
+              <MyMenu :val="2" @fromChild="getChildMsg">
+                <span slot="1">热门</span>
+                <span slot="2">电视影音</span>
+              </MyMenu>
+            </div>
+          </div>
+          <div class="box-hd">
+            <div class="promo-list">
+              <ul>
+                <li>
+                  <img :src="$target + 'public/imgs/appliance-promo1.png'" />
+                </li>
+              </ul>
+            </div>
+            <div class="list">
+              <MyList :list="applianceList" :isMore="true"></MyList>
+            </div>
+          </div>
+        </div>
+        <!-- HEA end -->
+
+        <!-- sundries -->
+        <div class="accessory" id="promo-menu">
+          <div class="box-hd">
+            <div class="title">配件</div>
+            <div class="more" id="more">
+              <MyMenu :val="3" @fromChild="getChildMsg2">
+                <span slot="1">热门</span>
+                <span slot="2">保护套</span>
+                <span slot="3">充电器</span>
+              </MyMenu>
+            </div>
+          </div>
+          <div class="box-bd">
+            <div class="promo-list">
+              <ul>
+                <li>
+                  <img :src="$target + 'public/imgs/accessory/accessory-promo1.png'" alt />
+                </li>
+                <li>
+                  <img :src="$target + 'public/imgs/accessory/accessory-promo2.png'" alt />
+                </li>
+              </ul>
+            </div>
+            <div class="list">
+              <MyList :list="accessoryList" :isMore="true"></MyList>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
+<script>
+export default {
+  data() {
+    return {
+      carousel: "", // 轮播图数据
+      phoneList: "", // 手机商品列表
+      miTvList: "", // 小米电视商品列表
+      applianceList: "", // 家电商品列表
+      applianceHotList: "", //热门家电商品列表
+      accessoryList: "", //配件商品列表
+      accessoryHotList: "", //热门配件商品列表
+      protectingShellList: "", // 保护套商品列表
+      chargerList: "", //充电器商品列表
+      applianceActive: 1, // 家电当前选中的商品分类
+      accessoryActive: 1 // 配件当前选中的商品分类
+    }
   },
-});
+  watch: {
+    applianceActive: function (val) {
+      if (this.applianceHotList == "") {
+        this.applianceHotList = this.applianceList;
+      }
+      if (val == 1) {
+        this.applianceList = this.applianceHotList;
+        return;
+      }
+      if (val == 2) {
+        this.applianceList = this.miTvList;
+        return;
+      }
+    },
+    accessoryActive: function (val) {
+      // 页面初始化的时候把accessoryHotList(热门配件商品列表)直接赋值给accessoryList(配件商品列表)
+      // 所以在切换商品列表时判断accessoryHotList是否为空,为空则是第一次切换,把accessoryList赋值给accessoryHotList
+      if (this.accessoryHotList == "") {
+        this.accessoryHotList = this.accessoryList;
+      }
+      if (val == 1) {
+        // 1为热门商品
+        this.accessoryList = this.accessoryHotList;
+        return;
+      }
+      if (val == 2) {
+        // 2为保护套商品
+        this.accessoryList = this.protectingShellList;
+        return;
+      }
+      if (val == 3) {
+        //3 为充电器商品
+        this.accessoryList = this.chargerList;
+        return;
+      }
+    }
+  },
+  created() {
+    // swiper
+    this.$axios
+      .post("/api/resources/carousel", {})
+      .then(res => {
+        this.carousel = res.data.carousel;
+      })
+      .catch(err => {
+        return Promise.reject(err);
+      });
+    this.getPromo("手机", "phoneList");
+    this.getPromo("电视机", "miTvList");
+    this.getPromo("保护套", "protectingShellList");
+    this.getPromo("充电器", "chargerList");
+    this.getPromo(
+      ["电视机", "空调", "洗衣机"],
+      "applianceList",
+      "/api/product/getHotProduct"
+    );
+    this.getPromo(
+      ["保护套", "保护膜", "充电器", "充电宝"],
+      "accessoryList",
+      "/api/product/getHotProduct"
+    );
+  },
+  methods: {
+    // get data from Child Components
+    getChildMsg(val) {
+      this.applianceActive = val;
+    },
+    getChildMsg2(val) {
+      this.accessoryActive = val;
+    },
+    getPromo(categoryName, val, api) {
+      api = api != undefined ? api : "/api/product/getPromoProduct";
+      this.$axios
+        .post(api, {
+          categoryName
+        })
+        .then(res => {
+          this[val] = res.data.Product;
+        })
+        .catch(err => {
+          return Promise.reject(err)
+        })
+    }
+  },
+}
 </script>
+<style scoped>
+@import "../assets/css/index.css";
+</style>
